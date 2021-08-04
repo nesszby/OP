bb80d4e6d6ee928382f74d270799c0b07917ea51
derekmini <mguangwe@gmail.com>
2021-08-04 16:53:14 +0800

    Update from original repo at 2021.08.04 16:50

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -6,8 +6,6 @@
 # 修改IP项的EOF于EOF之间请不要插入其他扩展代码，可以删除或注释里面原本的代码
 
 
-
-
 cat >$NETIP <<-EOF
 # uci set network.lan.ipaddr='10.1.1.1'                                       # IPv4 地址(openwrt后台地址)
 # uci set network.lan.netmask='255.255.255.0'                                 # IPv4 子网掩码
@@ -21,7 +19,6 @@ cat >$NETIP <<-EOF
 uci set system.@system[0].hostname='OP'                                     # 修改主机名称为OpenWrt-123
 EOF
 
-
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile           # 选择argon为默认主题
 
 sed -i "s/OpenWrt /${Author} Compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ          # 增加个性名字${Author}默认为你的github账号
@@ -32,7 +29,6 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0/$1$DvXOFLQQ$ReseTAbhIpWzBLwWU4hI8
 
 # K3专用，编译K3的时候只会出K3固件
 #sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
-
 
 # 修改插件名字
 sed -i 's/"aMule设置"/"电驴下载"/g' `grep "aMule设置" -rl ./`
